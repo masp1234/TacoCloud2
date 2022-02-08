96e8372b60451a5827d80e3ee0bfcc24397e3548
masp1234 <mart2c04@stud.kea.dk>
2022-02-08 19:07:08 +0100

    Second commit

---
+++ src/main/java/com/example/tacocloud2/TacoCloud2Application.java
@@ -0,0 +1,13 @@
+package com.example.tacocloud2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloud2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloud2Application.class, args);
+    }
+
+}
